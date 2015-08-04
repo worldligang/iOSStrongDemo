aabf616204039436794057f28e5175d91e969d77
worldligang <worldligang@163.com>
2015-08-04 22:35:21 +0800

    保存照片到自己创建的相簿

---
+++ Function/ALAssetsLibrary+CustomPhotoAlbum.m
@@ -0,0 +1,354 @@
+//
+//  ALAssetsLibrary category to handle a custom photo album
+//
+//  Created by Marin Todorov on 10/26/11.
+//  Copyright (c) 2011 Marin Todorov. All rights reserved.
+//
+
+#import "ALAssetsLibrary+CustomPhotoAlbum.h"
+
+#if !__has_feature(objc_arc)
+#error This class requires automatic reference counting (ARC).
+#endif
+
+@interface ALAssetsLibrary (Private)
+
+/*! A block wraper to be executed after asset adding process done. (Private)
+ *
+ * \param albumName Custom album name
+ * \param completion Block to be executed when succeed to add the asset to the assets library (camera roll)
+ * \param failure Block to be executed when failed to add the asset to the custom photo album
+ */
+- (ALAssetsLibraryWriteImageCompletionBlock)_resultBlockOfAddingToAlbum:(NSString *)albumName
+                                                             completion:(ALAssetsLibraryWriteImageCompletionBlock)completion
+                                                                failure:(ALAssetsLibraryAccessFailureBlock)failure;
+
+/*! A block wraper to be executed after |-assetForURL:resultBlock:failureBlock:| succeed.
+ *  Generally, this block will be excused when user confirmed the application's access
+ *    to the library.
+ *
+ * \param group A group to be used to add photo to the target album
+ * \param assetURL The URL for the target asset
+ * \param completion Block to be executed when succeed to add the asset to the assets library (camera roll)
+ * \param failure Block to be executed when failed to add the asset to the custom photo album
+ *
+ * \return An ALAssetsLibraryAssetForURLResultBlock type block
+ */
+- (ALAssetsLibraryAssetForURLResultBlock)_assetForURLResultBlockWithGroup:(ALAssetsGroup *)group
+                                                                 assetURL:(NSURL *)assetURL
+                                                               completion:(ALAssetsLibraryWriteImageCompletionBlock)completion
+                                                                  failure:(ALAssetsLibraryAccessFailureBlock)failure;
+
+@end
+
+
+@implementation ALAssetsLibrary (CustomPhotoAlbum)
+
+#pragma mark - Private Method
+
+- (ALAssetsLibraryWriteImageCompletionBlock)_resultBlockOfAddingToAlbum:(NSString *)albumName
+                                                             completion:(ALAssetsLibraryWriteImageCompletionBlock)completion
+                                                                failure:(ALAssetsLibraryAccessFailureBlock)failure
+{
+  return ^(NSURL *assetURL, NSError *error) {
+    // Run the completion block for writing image to saved
+    //   photos album
+    //if (completion) completion(assetURL, error);
+    
+    // If an error occured, do not try to add the asset to
+    //   the custom photo album
+    if (error != nil) {
+      if (failure) failure(error);
+      return;
+    }
+    
+    // Add the asset to the custom photo album
+    [self addAssetURL:assetURL
+              toAlbum:albumName
+           completion:completion
+              failure:failure];
+  };
+}
+
+- (ALAssetsLibraryAssetForURLResultBlock)_assetForURLResultBlockWithGroup:(ALAssetsGroup *)group
+                                                                 assetURL:(NSURL *)assetURL
+                                                               completion:(ALAssetsLibraryWriteImageCompletionBlock)completion
+                                                                  failure:(ALAssetsLibraryAccessFailureBlock)failure
+{
+  return ^(ALAsset *asset) {
+    // Add photo to the target album
+    if ([group addAsset:asset]) {
+      // Run the completion block if the asset was added successfully
+      if (completion) completion(assetURL, nil);
+    }
+    // |-addAsset:| may fail (return NO) if the group is not editable,
+    //   or if the asset could not be added to the group.
+    else {
+      NSString * message = [NSString stringWithFormat:@"ALAssetsGroup failed to add asset: %@.", asset];
+      if (failure) failure([NSError errorWithDomain:@"LIB_ALAssetsLibrary_CustomPhotoAlbum"
+                                               code:0
+                                           userInfo:@{NSLocalizedDescriptionKey : message}]);
+    }
+  };
+}
+
+#pragma mark - Public Method
+
+- (void)saveImage:(UIImage *)image
+          toAlbum:(NSString *)albumName
+       completion:(ALAssetsLibraryWriteImageCompletionBlock)completion
+          failure:(ALAssetsLibraryAccessFailureBlock)failure
+{
+  [self writeImageToSavedPhotosAlbum:image.CGImage
+                         orientation:(ALAssetOrientation)image.imageOrientation 
+                     completionBlock:[self _resultBlockOfAddingToAlbum:albumName
+                                                            completion:completion
+                                                               failure:failure]];
+}
+
+- (void)saveVideo:(NSURL *)videoUrl
+          toAlbum:(NSString *)albumName
+       completion:(ALAssetsLibraryWriteImageCompletionBlock)completion
+          failure:(ALAssetsLibraryAccessFailureBlock)failure
+{
+    [self writeVideoAtPathToSavedPhotosAlbum:videoUrl
+                             completionBlock:[self _resultBlockOfAddingToAlbum:albumName
+                                                                    completion:completion
+                                                                       failure:failure]];
+}
+
+- (void)saveImageData:(NSData *)imageData
+              toAlbum:(NSString *)albumName
+             metadata:(NSDictionary *)metadata
+           completion:(ALAssetsLibraryWriteImageCompletionBlock)completion
+              failure:(ALAssetsLibraryAccessFailureBlock)failure
+{
+  [self writeImageDataToSavedPhotosAlbum:imageData
+                                metadata:metadata
+                         completionBlock:[self _resultBlockOfAddingToAlbum:albumName
+                                                                completion:completion
+                                                                   failure:failure]];
+}
+
+- (void)addAssetURL:(NSURL *)assetURL
+            toAlbum:(NSString *)albumName
+         completion:(ALAssetsLibraryWriteImageCompletionBlock)completion
+            failure:(ALAssetsLibraryAccessFailureBlock)failure
+{
+  __block BOOL albumWasFound = NO;
+  
+  // Signature for the block executed when a match is found during enumeration using
+  //   |-enumerateGroupsWithTypes:usingBlock:failureBlock:|.
+  //
+  // |group|: The current asset group in the enumeration.
+  // |stop| : A pointer to a boolean value; set the value to YES to stop enumeration.
+  //
+  ALAssetsLibraryGroupsEnumerationResultsBlock enumerationBlock;
+  enumerationBlock = ^(ALAssetsGroup *group, BOOL *stop) {
+    // Compare the names of the albums
+    if ([albumName compare:[group valueForProperty:ALAssetsGroupPropertyName]] == NSOrderedSame) {
+      // Target album is found
+      albumWasFound = YES;
+      
+      // Get a hold of the photo's asset instance
+      // If the user denies access to the application, or if no application is allowed to
+      //   access the data, the failure block is called.
+      ALAssetsLibraryAssetForURLResultBlock assetForURLResultBlock =
+        [self _assetForURLResultBlockWithGroup:group
+                                      assetURL:assetURL
+                                    completion:completion
+                                       failure:failure];
+      [self assetForURL:assetURL
+            resultBlock:assetForURLResultBlock
+           failureBlock:failure];
+      
+      // Album was found, bail out of the method
+      *stop = YES;
+    }
+    
+    if (group == nil && albumWasFound == NO) {
+      // Photo albums are over, target album does not exist, thus create it
+      
+      // Since you use the assets library inside the block,
+      //   ARC will complain on compile time that there’s a retain cycle.
+      //   When you have this – you just make a weak copy of your object.
+      ALAssetsLibrary * __weak weakSelf = self;
+      
+      void(^addPhotoToLibraryBlock)(ALAssetsGroup *group) = ^void(ALAssetsGroup *group) {
+        // Get the photo's instance
+        //   add the photo to the newly created album
+        ALAssetsLibraryAssetForURLResultBlock assetForURLResultBlock =
+        [weakSelf _assetForURLResultBlockWithGroup:group
+                                          assetURL:assetURL
+                                        completion:completion
+                                           failure:failure];
+        [weakSelf assetForURL:assetURL
+                  resultBlock:assetForURLResultBlock
+                 failureBlock:failure];
+      };
+      
+      // If iOS version is lower than 5.0, throw a warning message
+      if (! [self respondsToSelector:@selector(addAssetsGroupAlbumWithName:resultBlock:failureBlock:)]) {
+        NSLog(@"%s: WARNING: |-addAssetsGroupAlbumWithName:resultBlock:failureBlock:| \
+              only available on iOS 5.0 or later. Asset cannot be saved to album.", __PRETTY_FUNCTION__);
+      }
+      // Create new assets album
+      else {
+        // Different code for iOS 7 and 8
+        // See: http://stackoverflow.com/questions/26003211/assetslibrary-framework-broken-on-ios-8
+        // See: http://stackoverflow.com/questions/8867496/get-last-image-from-photos-app/8872425#8872425
+        // PHPhotoLibrary_class will only be non-nil on iOS 8.0 or later.
+        Class PHPhotoLibrary_class = NSClassFromString(@"PHPhotoLibrary");
+        
+        if (PHPhotoLibrary_class) {
+          // dynamic runtime code for code chunk listed above
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+          id sharedPhotoLibrary = [PHPhotoLibrary_class performSelector:NSSelectorFromString(@"sharedPhotoLibrary")];
+#pragma clang diagnostic pop
+          
+          BOOL shouldInvokeSuccessBlockInMainThread = ([NSThread currentThread] == [NSThread mainThread]);
+          
+          SEL performChanges;
+          if (shouldInvokeSuccessBlockInMainThread) {
+            // Synchronously runs a block that requests changes to be performed in the Photos library
+            performChanges = NSSelectorFromString(@"performChangesAndWait:error:");
+          } else {
+            // Asynchronously runs a block that requests changes to be performed in the Photos library
+            performChanges = NSSelectorFromString(@"performChanges:completionHandler:");
+          }
+          
+          NSMethodSignature * methodSignature = [sharedPhotoLibrary methodSignatureForSelector:performChanges];
+          
+          NSInvocation * invocation = [NSInvocation invocationWithMethodSignature:methodSignature];
+          [invocation setTarget:sharedPhotoLibrary];
+          [invocation setSelector:performChanges];
+          
+          // Set the |changeBlock| for |-performChangesAndWait:error:| or |-performChanges:completionHandler:|.
+          void (^changeBlock)() = ^{
+            Class PHAssetCollectionChangeRequest_class = NSClassFromString(@"PHAssetCollectionChangeRequest");
+            SEL creationRequestForAssetCollectionWithTitle = NSSelectorFromString(@"creationRequestForAssetCollectionWithTitle:");
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+            [PHAssetCollectionChangeRequest_class performSelector:creationRequestForAssetCollectionWithTitle withObject:albumName];
+#pragma clang diagnostic pop
+          };
+          [invocation setArgument:&changeBlock atIndex:2];
+          
+          // Block to be invoked after created album succeed.
+          void (^blockToEnumerateGroups)() = ^{
+            [self enumerateGroupsWithTypes:ALAssetsGroupAlbum
+                                usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+                                  if (group) {
+                                    NSString *name = [group valueForProperty:ALAssetsGroupPropertyName];
+                                    if ([albumName isEqualToString:name]) {
+                                      addPhotoToLibraryBlock(group);
+                                    }
+                                  }
+                                }
+                              failureBlock:failure];
+          };
+          
+          // Setup invocation to perfom selector |-performChangesAndWait:error:| in main thread.
+          if (shouldInvokeSuccessBlockInMainThread) {
+            // Set error point for |-performChangesAndWait:error:|.
+            NSError * error = nil;
+            [invocation setArgument:&error atIndex:3];
+            [invocation invoke];
+            
+            // Get return value of |-performChangesAndWait:error:|.
+            BOOL createAlbumSucceed;
+            [invocation getReturnValue:&createAlbumSucceed];
+            
+            if (createAlbumSucceed) {
+              blockToEnumerateGroups();
+            } else {
+              if (error) {
+                NSLog(@"%s: Error creating album (%@) :  %@",
+                      __PRETTY_FUNCTION__, albumName, [error localizedDescription]);
+              }
+            }
+          }
+          // Setup invocation to perfom selector |-performChanges:completionHandler:| in non-main thread.
+          else {
+            void (^completionHandler)(BOOL success, NSError *error) = ^(BOOL success, NSError *error) {
+              if (success) {
+                blockToEnumerateGroups();
+              } else {
+                if (error) {
+                  NSLog(@"%s: Error creating album (%@) : %@",
+                        __PRETTY_FUNCTION__, albumName, [error localizedDescription]);
+                }
+              }
+            };
+            // Set the |completionHandler| for |-performChanges:completionHandler:|.
+            [invocation setArgument:&completionHandler atIndex:3];
+            [invocation invoke];
+          }
+        }
+        else {
+          // code that always creates an album on iOS 7.x.x but fails
+          // in certain situations such as if album has been deleted
+          // previously on iOS 8.x.
+          [self addAssetsGroupAlbumWithName:albumName
+                                resultBlock:addPhotoToLibraryBlock
+                               failureBlock:failure];
+        }
+      }
+      // Should be the last iteration anyway, but just in case
+      *stop = YES;
+    }
+  };
+  
+  // Search all photo albums in the library
+  [self enumerateGroupsWithTypes:ALAssetsGroupAlbum
+                      usingBlock:enumerationBlock
+                    failureBlock:failure];
+}
+
+- (void)loadImagesFromAlbum:(NSString *)albumName
+                 completion:(void (^)(NSMutableArray *, NSError *))completion
+{
+  ALAssetsLibraryGroupsEnumerationResultsBlock block = ^(ALAssetsGroup *group, BOOL *stop) {
+    // Checking if library exists
+    if (group == nil) {
+      *stop = YES;
+      return;
+    }
+    
+    // If we have found library with given title we enumerate it
+    if ([albumName compare:[group valueForProperty:ALAssetsGroupPropertyName]] == NSOrderedSame) {
+      NSMutableArray * images = [[NSMutableArray alloc] init];
+      [group enumerateAssetsUsingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
+        // Checking if group isn't empty
+        if (! result) return;
+        
+        // Getting the image from the asset
+        UIImageOrientation orientation =
+          (UIImageOrientation)[[result valueForProperty:@"ALAssetPropertyOrientation"] intValue];
+        UIImage * image = [UIImage imageWithCGImage:[[result defaultRepresentation] fullScreenImage]
+                                              scale:1.0
+                                        orientation:orientation];
+        // Saving this image to the array
+        [images addObject:image];
+      }];
+      
+      // Execute the |completion| block
+      if (completion) completion(images, nil);
+      
+      // Album was found, bail out of the method
+      *stop = YES;
+    }
+  };
+  
+  ALAssetsLibraryAccessFailureBlock failureBlock = ^(NSError *error) {
+    NSLog(@"%s: %@", __PRETTY_FUNCTION__, [error localizedDescription]);
+    if (completion) completion(nil, error);
+  };
+  
+  [self enumerateGroupsWithTypes:ALAssetsGroupAll
+                      usingBlock:block
+                    failureBlock:failureBlock];
+}
+
+@end
--- Function/GGAlbumViewController.m
+++ Function/GGAlbumViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "GGAlbumViewController.h"
+#import "ALAssetsLibrary+CustomPhotoAlbum.h"
 
 #define SCREEN [UIScreen mainScreen].bounds.size
 
@@ -23,7 +24,7 @@
     // Do any additional setup after loading the view.
     
     self.view.backgroundColor = [UIColor whiteColor];
-
+    
     
     self.image = [UIImage imageNamed:@"iOSDevTip"];
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake((SCREEN.width - 300) / 2, 70, 300, 150)];
@@ -37,9 +38,243 @@
     [button setBackgroundColor:[UIColor orangeColor]];
     [button setTitle:@"SavePhoto" forState:UIControlStateNormal];
     [self.view addSubview:button];
+    
+    UIButton *buttonSaveAlbum = [[UIButton alloc] init];
+    buttonSaveAlbum.frame = CGRectMake( 100, 360, SCREEN.width - 200, 40);
+    [buttonSaveAlbum addTarget:self action:@selector(createAlbum:) forControlEvents:UIControlEventTouchUpInside];
+    [buttonSaveAlbum setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [buttonSaveAlbum setBackgroundColor:[UIColor orangeColor]];
+    [buttonSaveAlbum setTitle:@"CreateAlbum" forState:UIControlStateNormal];
+    [self.view addSubview:buttonSaveAlbum];
+}
+
+- (void)createAlbum:(UIButton *)button
+{
+    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    [library saveImage:self.image toAlbum:@"gang" completion:^(NSURL *assetURL, NSError *error) {
+        if (!error) {
+            
+        }
+    } failure:^(NSError *error) {
+        if (error) {
+            
+        }
+    }];
+    
+//    [self getGroup];
+//    [self savedLibrayImage:self.image];
+    //添加到相册我让它执行是异步执行的方式，如果不想用这种方式，可以不去开一个线程
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        
+        [self createAlbum];
+        
+    });
+}
+
+#pragma mark - 创建相册
+- (void)createAlbum
+{
+    ALAssetsLibrary *assetsLibrary = [[ALAssetsLibrary alloc] init];
+    NSMutableArray *groups=[[NSMutableArray alloc]init];
+    ALAssetsLibraryGroupsEnumerationResultsBlock listGroupBlock = ^(ALAssetsGroup *group, BOOL *stop)
+    {
+        if (group)
+        {
+            [groups addObject:group];
+        }
+        
+        else
+        {
+            BOOL haveHDRGroup = NO;
+            
+            for (ALAssetsGroup *gp in groups)
+            {
+                NSString *name =[gp valueForProperty:ALAssetsGroupPropertyName];
+                
+                if ([name isEqualToString:@"iOSDevTip"])
+                {
+                    haveHDRGroup = YES;
+                }
+            }
+            
+            if (!haveHDRGroup)
+            {
+                //do add a group named "XXXX"
+                [assetsLibrary addAssetsGroupAlbumWithName:@"iOSDevTip"
+                                               resultBlock:^(ALAssetsGroup *group)
+                 {
+                     [groups addObject:group];
+                     
+                 }
+                                              failureBlock:nil];
+                haveHDRGroup = YES;
+            }
+        }
+        
+    };
+    //创建相簿
+    [assetsLibrary enumerateGroupsWithTypes:ALAssetsGroupAlbum usingBlock:listGroupBlock failureBlock:nil];
+    
+    [self saveToAlbumWithMetadata:nil imageData:UIImagePNGRepresentation(self.image) customAlbumName:@"iOSDevTip" completionBlock:^
+     {
+         //这里可以创建添加成功的方法
+         
+     }
+                     failureBlock:^(NSError *error)
+     {
+         //处理添加失败的方法显示alert让它回到主线程执行，不然那个框框死活不肯弹出来
+         dispatch_async(dispatch_get_main_queue(), ^{
+             
+             //添加失败一般是由用户不允许应用访问相册造成的，这边可以取出这种情况加以判断一下
+             if([error.localizedDescription rangeOfString:@"User denied access"].location != NSNotFound ||[error.localizedDescription rangeOfString:@"用户拒绝访问"].location!=NSNotFound){
+                 UIAlertView *alert=[[UIAlertView alloc]initWithTitle:error.localizedDescription message:error.localizedFailureReason delegate:nil cancelButtonTitle:NSLocalizedString(@"ok", nil) otherButtonTitles: nil];
+                 
+                 [alert show];
+             }
+         });
+     }];
+}
+
+- (void)saveToAlbumWithMetadata:(NSDictionary *)metadata
+                      imageData:(NSData *)imageData
+                customAlbumName:(NSString *)customAlbumName
+                completionBlock:(void (^)(void))completionBlock
+                   failureBlock:(void (^)(NSError *error))failureBlock
+{
+    
+    ALAssetsLibrary *assetsLibrary = [[ALAssetsLibrary alloc] init];
+    __weak ALAssetsLibrary *weakSelf = assetsLibrary;
+    void (^AddAsset)(ALAssetsLibrary *, NSURL *) = ^(ALAssetsLibrary *assetsLibrary, NSURL *assetURL) {
+        [assetsLibrary assetForURL:assetURL resultBlock:^(ALAsset *asset) {
+            [assetsLibrary enumerateGroupsWithTypes:ALAssetsGroupAll usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+                
+                if ([[group valueForProperty:ALAssetsGroupPropertyName] isEqualToString:customAlbumName]) {
+                    [group addAsset:asset];
+                    if (completionBlock) {
+                        completionBlock();
+                    }
+                }
+            } failureBlock:^(NSError *error) {
+                if (failureBlock) {
+                    failureBlock(error);
+                }
+            }];
+        } failureBlock:^(NSError *error) {
+            if (failureBlock) {
+                failureBlock(error);
+            }
+        }];
+    };
+    [assetsLibrary writeImageDataToSavedPhotosAlbum:imageData metadata:metadata completionBlock:^(NSURL *assetURL, NSError *error) {
+        if (customAlbumName) {
+            [assetsLibrary addAssetsGroupAlbumWithName:customAlbumName resultBlock:^(ALAssetsGroup *group) {
+                if (group) {
+                    [weakSelf assetForURL:assetURL resultBlock:^(ALAsset *asset) {
+                        [group addAsset:asset];
+                        if (completionBlock) {
+                            completionBlock();
+                        }
+                    } failureBlock:^(NSError *error) {
+                        if (failureBlock) {
+                            failureBlock(error);
+                        }
+                    }];
+                } else {
+                    AddAsset(weakSelf, assetURL);
+                }
+            } failureBlock:^(NSError *error) {
+                AddAsset(weakSelf, assetURL);
+            }];
+        } else {
+            if (completionBlock) {
+                completionBlock();
+            }
+        }
+    }];
 }
 
 
+-(void)savedLibrayImage:(UIImage *)image
+{
+    
+    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    [library addAssetsGroupAlbumWithName:@"test" resultBlock:^(ALAssetsGroup *group) {
+        
+        //创建相簿成功
+        
+    } failureBlock:^(NSError *error) {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"存储失败"
+                                                       message:@"请打开 设置-隐私-照片 来进行设置"
+                                                      delegate:nil
+                                             cancelButtonTitle:@"确定"
+                                             otherButtonTitles:nil, nil];
+        [alert show];
+    }];
+    
+    
+//    ALAssetsLibrary *library = [[ALAssetsLibrary alloc]init];
+//    [library writeImageToSavedPhotosAlbum:image.CGImage orientation:(ALAssetOrientation)image.imageOrientation completionBlock:^(NSURL *asSetUrl,NSError *error){
+//        if (error) {
+//            UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"存储失败"
+//                                                           message:@"请打开 设置-隐私-照片 来进行设置"
+//                                                          delegate:nil
+//                                                 cancelButtonTitle:@"确定"
+//                                                 otherButtonTitles:nil, nil];
+//            [alert show];
+//        }else{
+//            UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"存储成功"
+//                                                           message:nil
+//                                                          delegate:nil
+//                                                 cancelButtonTitle:@"确定"
+//                                                 otherButtonTitles:nil, nil];
+//            [alert show];
+//            
+//        }
+//    }];
+    
+}
+
+
+
+-(void)getGroup
+{
+    @autoreleasepool
+    {
+        NSMutableArray *groupArray = [[NSMutableArray alloc] init];
+
+        ALAssetsLibraryAccessFailureBlock failureblock =
+        ^(NSError *myerror)
+        {
+            NSLog(@"相册访问失败 =%@", [myerror localizedDescription]);
+            if ([myerror.localizedDescription rangeOfString:@"Global denied access"].location!=NSNotFound) {
+                NSLog(@"无法访问相册.请在'设置->定位服务'设置为打开状态.");
+            }else{
+                NSLog(@"相册访问失败.");
+            }
+        };
+        
+        ALAssetsLibraryGroupsEnumerationResultsBlock
+        libraryGroupsEnumeration = ^(ALAssetsGroup* group,BOOL* stop)
+        {
+            if (group!=nil)
+            {
+                [groupArray addObject:group];
+            }
+            else
+            {
+//                if (!_groupTable)
+//                {
+//                    _groupTable = [[UITableView alloc] initWithFrame:EZRECT(0, 0, SCREEN_SIZE_WIDTH, SCREEN_SIZE_HEIGHT - 44)
+//                                                               style:UITableViewStylePlain];
+//                    _groupTable.delegate = self;
+//                    _groupTable.dataSource = self;
+//                    [self.view addSubview:_groupTable];
+//                }
+//                [_groupTable performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:NO];
+            }
+        };
+    }
+}
 
 - (void)actionClick:(UIButton *)button
 {
